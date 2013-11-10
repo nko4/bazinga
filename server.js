a43fb607c628756b9845fcc5d4e448d0f85e4eff
Luis Reis <luis.m.reis@gmail.com>
2013-11-10 02:33:56 +0000

    Move mac vmstat to its own file. Use uname to check system.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var fs = require('fs');
 var restify = require('restify');
 var socketio = require('socket.io');
 var carrier = require('carrier');
+var _ = require('lodash');
+var Handlebars = require('handlebars');
 
 var EventEmitter = require('events').EventEmitter;
 var spawn = require('child_process').spawn;
@@ -13,84 +15,63 @@ process.on('uncaughtException', function(err) {
   console.log('Caught exception: ' + err);
 });
 
-var isProduction = (process.env.NODE_ENV === 'production');
-var port = (isProduction ? 80 : 8000);
-
-var server = restify.createServer();
-var io = socketio.listen(server);
-
-server.get(/\/.*/, restify.serveStatic({
-  directory: './web',
-  default: 'index.html'
-}));
-
-function createMacVmStatConfig(updateInterval) {
-  function MacVmStatConfig() {
-    this.command = 'vm_stat';
-    this.args = [updateInterval];
-    this.headers = null;
-    this._lastWasHeader = false;
-    this.ignorePatterns = [/Mach Virtual Memory Statistics/];
-    this.renderingConfig = {
-      order: ['totals', 'pages', 'compressor', 'swap'],
-      charts: {
-        totals: {
-          type: 'stacked',
-          fields: ['free', 'active', 'inactive', 'specul', 'throttle', 'wired', 'prgable'],
-        },
-        pages: {
-          type: 'line',
-          fields: ['faults', 'copy', '0fill', 'reactive', 'purged', 'file-backed', 'anonymous']
-        },
-        compressor: {
-          type: 'line',
-          fields: ['cmprssed', 'cmprssor', 'dcomprs', 'comprs']
-        },
-        swap: {
-          type: 'line',
-          fields: ['pageins', 'pageout', 'swapins', 'swapouts']
-        }
-      }
-    };
-  }
-
-  return new MacVmStatConfig();
-}
-
-var updateInterval = 1;
-var config = createMacVmStatConfig(updateInterval);
-
-var statEmitter = new EventEmitter();
-
-io.set('log level', isProduction ? 1 : 2);
-io.sockets.on('connection', function (socket) {
-  socket.emit('config', config.renderingConfig);
-
-  function sendSample(sample) {
-    socket.emit('sample', sample);
-  }
-  statEmitter.on('sample', sendSample)
-
-  socket.on('disconnect', function () {
-    statEmitter.removeListener('sample', sendSample);
+var profiles = fs.readdirSync(__dirname + '/lib/profiles').map(function (filename) {
+  return _.tap(require('./lib/profiles/' + filename), function (profile) {
+    profile.unamePattern = new RegExp(profile.unamePattern);
   });
 });
 
-server.listen(port, function (err) {
-  if (err) { console.error(err); process.exit(-1); }
+function createConfig(uname, options) {
+  var config = _.cloneDeep(_.find(profiles, function (profile) {
+    return profile.tool === options.tool &&
+           uname.match(profile.unamePattern);
+  }));
 
-  // if run as root, downgrade to the owner of this file
-  if (process.getuid() === 0) {
-    require('fs').stat(__filename, function(err, stats) {
-      if (err) { return console.error(err); }
-      process.setuid(stats.uid);
+  if (!config) return undefined;
+
+  config.args = config.args.map(function (arg) {
+    return Handlebars.compile(arg)(options);
+  });
+
+  return config;
+}
+
+function setupSocketIO(server, config, isProduction, statEmitter) {
+  var io = socketio.listen(server);
+  io.set('log level', isProduction ? 1 : 2);
+  io.sockets.on('connection', function (socket) {
+    socket.emit('config', config.renderingConfig);
+
+    function sendSample(sample) {
+      socket.emit('sample', sample);
+    }
+    statEmitter.on('sample', sendSample)
+
+    socket.on('disconnect', function () {
+      statEmitter.removeListener('sample', sendSample);
     });
-  }
+  });
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
-});
+  return io;
+}
 
-function lineParser(config) {
+function runServer(server, port) {
+  server.listen(port, function (err) {
+    if (err) { console.error(err); process.exit(-1); }
+
+    // if run as root, downgrade to the owner of this file
+    if (process.getuid() === 0) {
+      require('fs').stat(__filename, function(err, stats) {
+        if (err) { return console.error(err); }
+        process.setuid(stats.uid);
+      });
+    }
+
+    console.log('Server running at http://0.0.0.0:' + port + '/');
+  });
+}
+
+function lineParser(config, statEmitter) {
   return function lineParser(line) {
     if (config.ignorePatterns.some(function(pattern) { return !!line.match(pattern); })) { return; }
 
@@ -113,5 +94,47 @@ function lineParser(config) {
   };
 }
 
-var p = spawn(config.command, config.args);
-carrier.carry(p.stdout, lineParser(config));
+function runUname(callback) {
+  var unameProcess = spawn('uname', ['-a']);
+  var output = '';
+  unameProcess.stdout.on('data', function (data) {
+    output += data;
+  });
+  unameProcess.on('close', function () {
+    uname = output;
+    callback(uname);
+  });
+}
+
+function spawnTool(config, statEmitter) {
+  var p = spawn(config.command, config.args);
+  carrier.carry(p.stdout, lineParser(config, statEmitter));
+}
+
+runUname(function (uname) {
+  var config = createConfig(uname,
+                            { updateInterval: 1,
+                              tool: 'vmstat' });
+
+  if (!config) {
+    console.log('Tool not found');
+    process.exit(1);
+  }
+
+  var isProduction = (process.env.NODE_ENV === 'production');
+  var port = (isProduction ? 80 : 8000);
+
+  var statEmitter = new EventEmitter();
+
+  var server = restify.createServer();
+
+  server.get(/\/.*/, restify.serveStatic({
+    directory: './web',
+    default: 'index.html'
+  }));
+
+  setupSocketIO(server, config, isProduction, statEmitter);
+  runServer(server, port);
+
+  spawnTool(config, statEmitter);
+});
